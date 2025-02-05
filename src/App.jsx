f1aa7d2e19bf2ea9b22387a6f89cd8efb4823248
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-02-05 15:17:11 +0000

    parse event data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ScheduleMeeting } from "react-schedule-meeting";
 import { subscribe, unsubscribe } from "./events";
 
 function App() {
-
-  const [ meetingData, setMeetingData ] = useState([]);
+  
+  const timeslots = [];
+  const [meetingData, setMeetingData] = useState([]);
 
   useEffect(() => {
     subscribe("appointmentsFound", (e) => setMeetingData(e.detail));
@@ -18,20 +19,32 @@ function App() {
     };
   });
 
-  if(meetingData) {
-    console.log("data received:", meetingData);
+  if (meetingData.length > 0) {
+    meetingData.data.forEach(day => {
+      Object.keys(day.appointments).forEach(unixTime => {
+        if(day.appointments[unixTime] === "available") {
+          const startTime = new Date(unixTime * 1000);
+          const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
+          timeslots.push({ id: unixTime, startTime, endTime });
+        }
+      })
+    })
   }
 
   return (
     <>
-      <ScheduleMeeting
-        borderRadius={10}
-        primaryColor="#03a9f4"
-        eventDurationInMinutes={30}
-        availableTimeslots={availableTimeslots}
-        onStartTimeSelect={console.log}
-        startTimeListStyle="scroll-list"
-      />
+      {meetingData.length > 0 ? (
+        <ScheduleMeeting
+          borderRadius={10}
+          primaryColor="#03a9f4"
+          eventDurationInMinutes={30}
+          availableTimeslots={timeslots}
+          onStartTimeSelect={console.log}
+          startTimeListStyle="scroll-list"
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
