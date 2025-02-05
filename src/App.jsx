fbe55d5a1fe46deab3e4c8e67e89cdeae37e2304
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-02-05 15:41:45 +0000

    3

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,27 @@ import { subscribe, unsubscribe } from "./events";
 function App() {
 
   const timeslots = [];
-  const [meetingData, setMeetingData] = useState([]);
+  const [meetingData, setMeetingData] = useState();
 
   useEffect(() => {
     subscribe("appointmentsFound", (e) => setMeetingData(e.detail));
-
-    meetingData?.data?.forEach(day => {
-      Object.keys(day?.appointments).forEach(unixTime => {
-        if(day?.appointments[unixTime] === "available") {
-          const startTime = new Date(unixTime * 1000);
-          const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
-          timeslots.push({ id: unixTime, startTime, endTime });
-        }
+    if (meetingData) {
+      meetingData?.data?.forEach(day => {
+        Object.keys(day?.appointments).forEach(unixTime => {
+          if(day?.appointments[unixTime] === "available") {
+            const startTime = new Date(unixTime * 1000);
+            const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
+            timeslots.push({ id: unixTime, startTime, endTime });
+          }
+        })
       })
-    })
-
+      console.log(timeslots);
+    }
   }, [meetingData]);
 
   return (
     <>
-      {meetingData.length > 0 ? (
+      {meetingData.length() > 0 ? (
         <ScheduleMeeting
           borderRadius={10}
           primaryColor="#03a9f4"
