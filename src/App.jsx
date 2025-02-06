786bc5fec76bd0bec8208e43c72e448011991615
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-02-06 14:38:51 +0000

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,43 @@ const ScheduleMeetingComponent = ({ sid }) => {
   const [timeslots, setTimeSlots] = useState([]);
   console.log(sid);
 
-
-  if (sid) {
-    const { data, error, isLoading } = useSWR(suggestAppointment(sid));
-    if (data) {
-      console.log(data);
-      const newTimeslots = [];
-      data.integration.transformed.rows_data[0].response.data.forEach(day => {
-        Object.keys(day?.appointments).forEach(unixTime => {
-          if (day?.appointments[unixTime] === "available") {
-            const startTime = new Date(unixTime * 1000);
-            const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
-            newTimeslots.push({ id: parseInt(unixTime), startTime, endTime });
-          }
+  useEffect((sid) => {
+    if (sid) {
+      const { data, error, isLoading } = useSWR(suggestAppointment(sid));
+      console.log("data", data);
+      console.log("error", error);
+      if (data) {
+        console.log(data);
+        const newTimeslots = [];
+        data.integration.transformed.rows_data[0].response.data.forEach(day => {
+          Object.keys(day?.appointments).forEach(unixTime => {
+            if (day?.appointments[unixTime] === "available") {
+              const startTime = new Date(unixTime * 1000);
+              const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
+              newTimeslots.push({ id: parseInt(unixTime), startTime, endTime });
+            }
+          });
         });
-      });
-      setTimeSlots(newTimeslots);
+        setTimeSlots(newTimeslots);
+      }
     }
-  }
+  }, [timeslots])
+
 
   return (
     <>
-        {timeslots.length > 0 ? (
-          <ScheduleMeeting
-            borderRadius={10}
-            primaryColor="#03a9f4"
-            eventDurationInMinutes={15}
-            availableTimeslots={timeslots}
-            onStartTimeSelect={createProvisional(startTimeEventEmit, sid)}
-            startTimeListStyle="scroll-list"
-          />
-        ) : (
-          <p>Loading...</p>
-        )}
+      {timeslots.length > 0 ? (
+        <ScheduleMeeting
+          borderRadius={10}
+          primaryColor="#03a9f4"
+          eventDurationInMinutes={15}
+          availableTimeslots={timeslots}
+          onStartTimeSelect={createProvisional(startTimeEventEmit, sid)}
+          startTimeListStyle="scroll-list"
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
