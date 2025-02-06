ac7b1e6d78064d45f6e2af48fe424186faa54947
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-02-06 09:15:25 +0000

    Removed meetingData state

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,33 @@ import { subscribe, unsubscribe } from "./events";
 
 function App() {
 
-  const [ meetingData, setMeetingData] = useState();
-  const [ timeslots, setTimeSlots] = useState([]);
+  const [timeslots, setTimeSlots] = useState([]);
 
   useEffect(() => {
     const handleAppointmentsFound = (e) => {
       console.log("Event received:", e.detail);
-      setMeetingData(e.detail);
-    };
-  
-    subscribe("appointmentsFound", handleAppointmentsFound);
-  
-    return () => {
-      unsubscribe("appointmentsFound", handleAppointmentsFound);
-    };
-  }, []);
 
-  useEffect(() => {
-    if (meetingData && typeof meetingData.data !== "undefined" && typeof meetingData.data !== null) {
       const newTimeslots = [];
-      meetingData.data.forEach(day => {
+      e.detail.data.forEach(day => {
         console.log("day", day);
         Object.keys(day?.appointments).forEach(unixTime => {
           if (day?.appointments[unixTime] === "available") {
             const startTime = new Date(unixTime * 1000);
             const endTime = new Date(startTime.getTime() + 30 * 60 * 1000); // Assuming 30 minutes duration
             newTimeslots.push({ id: parseInt(unixTime), startTime, endTime });
-            console.log({id: parseInt(unixTime), startTime, endTime});
+            console.log({ id: parseInt(unixTime), startTime, endTime });
           }
         });
       });
       setTimeSlots(newTimeslots);
-    }
-  }, [meetingData]);
+    };
+
+    subscribe("appointmentsFound", handleAppointmentsFound);
+
+    return () => {
+      unsubscribe("appointmentsFound", handleAppointmentsFound);
+    };
+  }, [timeslots]);
 
   return (
     <>
