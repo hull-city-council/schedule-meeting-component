b3b014a7bfb1daed2587e3fb87fdd37922688a03
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-02-07 09:12:31 +0000

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { ScheduleMeeting } from "react-schedule-meeting";
-import { suggestAppointment, createProvisional } from "./lookups";
+import { createProvisional } from "./lookups";
 
 
 const ScheduleMeetingComponent = ({ sid }) => {
 
   const [timeslots, setTimeSlots] = useState([]);
   const [data, setData] = useState(null);
-  console.log(sid);
+  
+  async function suggestAppointment(sid) {
+    try {
+      await fetch("/apibroker/?api=RunLookup&app_name=AchieveForms&sid=" + sid + "&id=63e50558b8a6f", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          formValues: {
+            Section1: {}
+          },
+          tokens: {
+            calendar_id: "cal_ZBLBiMdhsAC-SWx@_0fGt7JIj9-gZFyXKf7Lcnw",
+            duration: 30,
+            start_time: "09:00",
+            end_time: "17:30",
+            granularity: 15,
+            event_location: "Telephone appointment",
+            event_ID: "FS684941349",
+            event_ID2: "FS684941349",
+            timezone: "Europe/London",
+            from: "2025-02-14",
+            to: "2025-02-24"
+          }
+        })
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          setData(data.integration.transformed.rows_data[0].response);
+        });
+  
+    } catch (error) {
+      console.error(error);
+      alert("Unable to fetch availbility");
+    }
+  }
 
   function processAppointmentDates(){
     console.log(data);
@@ -28,7 +68,6 @@ useEffect(() => {
   if (sid) {
     async function fetchSuggestedAppointments() {
       const result = await suggestAppointment(sid);
-      setData(result);
       console.log("data", result);
       if (result) {
         processAppointmentDates();
